c063a95ff30779b2c307e065ea271218871d9c5d
amercado <arnold.mercado@hotmail.com>
2024-01-29 06:04:06 +0300

    Add Jwt

--- ChoiceDinner.Api/Program.cs
+++ ChoiceDinner.Api/Program.cs
@@ -7,7 +7,8 @@ using ChoiceDinner.Application;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddInfrastructure(builder.Configuration)
+            .AddJwtService(builder.Configuration);
 builder.Services.AddApplication();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
--- ChoiceDinner.Infrastructure/DependecyInjection.cs
+++ ChoiceDinner.Infrastructure/DependecyInjection.cs
@@ -1,25 +1,25 @@
 using ChoiceDinner.Domain.Models.Users;
 using ChoiceDinner.Infrastructure.Configurations;
 using ChoiceDinner.Infrastructure.Context;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 
 
 namespace ChoiceDinner.Infrastructure;
 
 public static class DependecyInjection
 {
-
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
-
-
-        services.AddDbContext<DinnerContext>(options => {
+        services.AddDbContext<DinnerContext>(options =>
+        {
             var efConfig = new EFConfiguration();
             configuration.GetSection(nameof(EFConfiguration)).Bind(efConfig);
 
-            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"), 
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
                                 action => action.CommandTimeout(efConfig.CommandTimeout));
             options.EnableSensitiveDataLogging(efConfig.EnableSensitiveDataLogging);
             options.EnableDetailedErrors(efConfig.EnableDetailedErrors);
@@ -32,6 +32,25 @@ public static class DependecyInjection
         }).AddEntityFrameworkStores<DinnerContext>();
 
 
+        return services;
+    }
+
+    public static IServiceCollection AddJwtService(this IServiceCollection services, IConfiguration configuration)
+    {
+
+        var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(configuration.GetValue<string>("TokenKey") ?? "secret tokent"));
+        
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(opt => {
+                opt.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = key,
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+        
         return services;
     }
 }
